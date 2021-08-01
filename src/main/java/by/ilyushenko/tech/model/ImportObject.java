22883bc72d0a7f2ab5aa823bc4236af428c7154a
diocsin <30667165+diocsin@users.noreply.github.com>
2021-08-01 19:03:38 +0300

    new Generator id for entity

--- src/main/java/by/ilyushenko/tech/model/ImportObject.java
+++ src/main/java/by/ilyushenko/tech/model/ImportObject.java
@@ -1,15 +1,32 @@
 package by.ilyushenko.tech.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 public class ImportObject {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GenericGenerator(
+            name = "ID_GENERATOR_POOLED",
+            strategy = "enhanced-sequence",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(
+                            name = "sequence_name",
+                            value = "JPWH_SEQUENCE"
+                    ),
+                    @org.hibernate.annotations.Parameter(
+                            name = "increment_size",
+                            value = "1000"
+                    ),
+                    @org.hibernate.annotations.Parameter(
+                            name = "optimizer",
+                            value = "pooled-lo"
+                    )
+            }
+    )
+    @GeneratedValue(generator = "ID_GENERATOR_POOLED")
     private Long pid;
 
     private String eventName;
@@ -58,5 +75,29 @@ public class ImportObject {
 
     public void setPid(Long pid) {
         this.pid = pid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ImportObject)) return false;
+        ImportObject that = (ImportObject) o;
+        return Objects.equals(pid, that.pid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pid);
+    }
+
+    @Override
+    public String toString() {
+        return "ImportObject{" +
+                "pid=" + pid +
+                ", eventName='" + eventName + '\'' +
+                ", formName='" + formName + '\'' +
+                ", variableName='" + variableName + '\'' +
+                ", value='" + value + '\'' +
+                '}';
     }
 }
