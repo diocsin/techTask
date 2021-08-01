815a1d13c862d0444882f89c1dee5559b0c812da
diocsin <30667165+diocsin@users.noreply.github.com>
2021-08-01 19:33:15 +0300

    change converterUtil

--- src/main/java/by/ilyushenko/tech/util/ConverterCsv.java
+++ src/main/java/by/ilyushenko/tech/util/ConverterCsv.java
@@ -1,24 +1,16 @@
 package by.ilyushenko.tech.util;
 
+import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.Reader;
 
 public class ConverterCsv {
 
-    public static <T> List<T> convertCsv(Class<T> tClass, String fileName) {
-        List<T> beans = new ArrayList<>();
-        try {
-            beans = new CsvToBeanBuilder(new FileReader(fileName))
-                    .withType(tClass)
-                    .build()
-                    .parse();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        return beans;
+    @SuppressWarnings("unchecked")
+    public static <T> CsvToBean<T> getCsvBean(final Class<T> clazz, final Reader reader) {
+        return new CsvToBeanBuilder(reader)
+                .withType(clazz)
+                .build();
     }
 }
