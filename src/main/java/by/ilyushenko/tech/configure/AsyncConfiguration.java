cd6609c20c0871948fba20820d28b85809aaee8c
diocsin <30667165+diocsin@users.noreply.github.com>
2021-08-02 18:14:02 +0300

    move config to config dir

--- src/main/java/by/ilyushenko/tech/configure/AsyncConfiguration.java
+++ src/main/java/by/ilyushenko/tech/configure/AsyncConfiguration.java
@@ -1,4 +1,4 @@
-package by.ilyushenko.tech;
+package by.ilyushenko.tech.configure;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
