f03316fb58763b2dca42db9d05f755868c166aa3
diocsin <30667165+diocsin@users.noreply.github.com>
2021-08-02 18:14:52 +0300

    fix exception

--- src/main/java/by/ilyushenko/tech/service/ImportObjectService.java
+++ src/main/java/by/ilyushenko/tech/service/ImportObjectService.java
@@ -2,14 +2,12 @@ package by.ilyushenko.tech.service;
 
 import by.ilyushenko.tech.model.ImportObject;
 
-import java.io.IOException;
-import java.sql.SQLException;
 import java.util.List;
 
 public interface ImportObjectService {
 
     List<ImportObject> findImportObject(final int offset, final int limit, final String filter);
 
-    void saveImportObjects(List<ImportObject> importObjects) throws IOException, SQLException;
+    void saveImportObjects(List<ImportObject> importObjects);
 
 }
