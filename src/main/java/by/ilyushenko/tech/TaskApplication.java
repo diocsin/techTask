37f67109243e291e8382672db83b1911b8371777
diocsin <30667165+diocsin@users.noreply.github.com>
2021-08-02 18:13:28 +0300

    clear Application main classes

--- src/main/java/by/ilyushenko/tech/TaskApplication.java
+++ src/main/java/by/ilyushenko/tech/TaskApplication.java
@@ -1,26 +1,70 @@
 package by.ilyushenko.tech;
 
-import by.ilyushenko.tech.model.ImportObject;
-import by.ilyushenko.tech.service.ImportObjectService;
-import by.ilyushenko.tech.util.ConverterCsv;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.file.FileReadingMessageSource;
+import org.springframework.integration.file.filters.ChainFileListFilter;
+import org.springframework.integration.file.filters.FileSystemPersistentAcceptOnceFileListFilter;
+import org.springframework.integration.file.filters.RegexPatternFileListFilter;
+import org.springframework.integration.metadata.PropertiesPersistingMetadataStore;
+import org.springframework.messaging.MessageChannel;
+
+import java.io.File;
 
 @SpringBootApplication
-public class TaskApplication implements CommandLineRunner {
+public class TaskApplication {
 
-    @Autowired
-    private ImportObjectService importObjectService;
+    @Value("${path.new.files}")
+    private String pathNewFiles;
+
+    @Value("${path.processed.files}")
+    private String pathProcessedFiles;
 
     public static void main(String[] args) {
         SpringApplication.run(TaskApplication.class, args);
-
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        importObjectService.saveImportObjects(ConverterCsv.convertCsv(ImportObject.class, "myFile0.csv"));
+    @Bean
+    public IntegrationFlow processFileFlow() {
+        return IntegrationFlows
+                .from("fileInputChannel")
+                .handle("fileProcessor", "process").get();
+    }
+
+    @Bean
+    public MessageChannel fileInputChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    @InboundChannelAdapter(value = "fileInputChannel", poller = @Poller(fixedDelay = "1000"))
+    public MessageSource<File> fileReadingMessageSource() {
+        ChainFileListFilter<File> filterCsv = new ChainFileListFilter<>();
+        filterCsv.addFilter(new RegexPatternFileListFilter("^.*\\.(csv|json)$"));
+
+        FileSystemPersistentAcceptOnceFileListFilter persistent = new FileSystemPersistentAcceptOnceFileListFilter(store(), "dailyfilesystem");
+        persistent.setFlushOnUpdate(true);
+        filterCsv.addFilter(persistent);
+
+        FileReadingMessageSource source = new FileReadingMessageSource();
+        source.setAutoCreateDirectory(true);
+        source.setDirectory(new File(pathNewFiles));
+        source.setFilter(filterCsv);
+        return source;
+    }
+
+    @Bean
+    public PropertiesPersistingMetadataStore store() {
+        PropertiesPersistingMetadataStore store = new PropertiesPersistingMetadataStore();
+        store.setBaseDirectory(pathProcessedFiles);
+        return store;
     }
 }
